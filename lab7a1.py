7f549adfc93b87be0b97a59b1bf1c2b126f2aed0
Onyedikachi Oji <ocoji@myseneca.ca>
2025-11-14 22:15:34 -0500

    Added lab7a1

--- lab7a1.py
+++ lab7a1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-# Student ID: [seneca_id]
+# Student ID: ocoji
 from lab7a import *
+
 t1 = Time(8,0,0)
 t2 = Time(8,55,0)
 t3 = Time(9,50,0)
@@ -9,7 +10,7 @@ td = Time(0,50,0)
 
 tsum1 = sum_times(t1,td)
 tsum2 = sum_times(t2,td)
-tsum3 = sum_times(t3,td)   
+tsum3 = sum_times(t3,td)
 
 ft = format_time
 print(ft(t1),'+',ft(td),'-->',ft(tsum1))
